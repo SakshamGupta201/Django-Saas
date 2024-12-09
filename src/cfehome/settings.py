ea805c6077a5f6f10f6b6e0048e3b0102f66c8b1
Saksham Gupta <guptasaksham495@gmail.com>
2024-12-09 23:12:19 +0530

    Refactor settings.py to use environment variables for SECRET_KEY, DEBUG, and ALLOWED_HOSTS

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_w4!6kc6j=d@4z!8vq+)^ayg9)as%(@a&vyd&99vrd3hlc*l19'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-_w4!6kc6j=d@4z!8vq+)^ayg9)as%(@a&vyd&99vrd3hlc*l19')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 if DEBUG == False:
-    ALLOWED_HOSTS = [
-        '.railway.app',
-    ]
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.railway.app').split(',')
 else: 
     ALLOWED_HOSTS = []
 
